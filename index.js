7eca28f9b8f0fd2466ca5035d61fc19d68f5bff2
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-02-28 13:07:49 +0000

    placed questions inside of addManager function

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 // import
 
-// classes
+// team classes
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
-// dependencies
+
+//  node dependencies
 const inquirer = require("inquirer");
 const fs = require("fs");
-//
+
+// link to create team HTML file
 const createTeam = require("./src/createTeam");
 
+// team array
 const teamArr = [];
 
 // manager questions
-const managerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the manager's name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the manager's Id?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the manager's email?",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is the manager's office number?",
-  },
-];
+
+const addManager = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's Id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team manager's office number?",
+    },
+  ]);
+};
