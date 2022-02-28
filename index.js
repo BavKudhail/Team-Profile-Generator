f33cd77132ffa2707badeba07f0a4be0091450f3
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-02-28 13:00:14 +0000

    Created questions array for manager

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ const fs = require("fs");
 const createTeam = require("./src/createTeam");
 
 const teamArr = [];
+
+// manager questions
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the manager's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the manager's Id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the manager's email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office number?",
+  },
+];
