0fbd41d4bb08882e2899219ec74acf2f22ca0e59
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-02-28 14:37:36 +0000

    Created tests for engineer class

---
+++ test/Engineer.test.js
@@ -0,0 +1,18 @@
+const Engineer = require("../lib/Engineer");
+
+// test creates engineer object
+test("creates engineer object", () => {
+  const github = "bavkudhail";
+  const engineer = new Engineer("Bav", "1996", "bavkudhail@gmail.com", github);
+
+  //   test name, id and email
+  expect(engineer.name).toEqual("Bav");
+  expect(engineer.id).toEqual("1996");
+  expect(engineer.email).toEqual("bavkudhail@gmail.com");
+
+  //   github
+  expect(engineer.getGithub()).toBe(github);
+
+  //   get role
+  expect(engineer.getRole()).toBe("Engineer");
+});
