c382b0af91384ed5fc3ea5671641377e46dbbbfa
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-02-28 14:41:35 +0000

    Created tests for intern class

---
+++ test/Intern.test.js
@@ -0,0 +1,15 @@
+const Intern = require("../lib/Intern");
+
+test("creating new intern object", () => {
+  const intern = new Intern("Bav", "1996", "bavkudhail@gmail.com", "MIT");
+  //   intern name
+  expect(intern.name).toBe("Bav");
+  //   intern ID
+  expect(intern.id).toBe("1996");
+  //   intern email
+  expect(intern.email).toBe("bavkudhail@gmail.com");
+  //    intern school
+  expect(intern.getSchool()).toBe("MIT");
+  //   intern role
+  expect(intern.getRole()).toBe("Intern");
+});
