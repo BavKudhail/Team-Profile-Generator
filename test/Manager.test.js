834ce2ba00c04ef542b521a566f5812bc329cb0f
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-02-28 14:21:01 +0000

    Manager getOfficeNumber method passed test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,8 +1,22 @@
 const { expect } = require("@jest/globals");
 const { it } = require("jest-circus");
+const { any } = require("micromatch");
 const { describe } = require("yargs");
 const Manager = require("../lib/Manager");
 
+// testing create manager object
+test("creates manager object", () => {
+  const officeNumber = 19;
+  const manager = new Manager(
+    "Bav",
+    "1996",
+    "bavkudhail@gmail.com",
+    officeNumber
+  );
+  expect(manager.getOfficeNumber()).toEqual(officeNumber);
+});
+
+// testing get role
 test("gets role", () => {
   const manager = new Manager("Bav", "1996", "bavkudhail@gmail.com", "19");
   expect(manager.getRole()).toEqual("Manager");
